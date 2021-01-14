eb0577391397f45043eef1cbbd93b58569c79489
ayato-ininin <sarai.a.1203408@gmail.com>
2021-01-14 12:05:45 +0900

    ad

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,14 @@ use Illuminate\Support\Facades\Auth;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('top');
 });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+$this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
